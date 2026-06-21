0128f9720729b88fc04443ad43f7878fefda4ef6
Matt <m.j.swales@hotmail.com>
2026-06-21 14:01:33 +0900

    Add files via upload

---
+++ campgrounds.js
@@ -0,0 +1,306 @@
+module.exports = [
+    {
+        name: "Fumotoppara Campground",
+        location: "Shizuoka, Japan",
+        latitude: 35.333,
+        longitude: 138.617
+    },
+    {
+        name: "Kouan Campsite (Lake Motosu)",
+        location: "Yamanashi, Japan",
+        latitude: 35.447,
+        longitude: 138.614
+    },
+    {
+        name: "Lake Shikotsu Campsite",
+        location: "Hokkaido, Japan",
+        latitude: 42.785,
+        longitude: 141.392
+    },
+    {
+        name: "Shiretoko Campsite (Rausu Area)",
+        location: "Hokkaido, Japan",
+        latitude: 44.029,
+        longitude: 145.128
+    },
+    {
+        name: "Lake Toya Campsite",
+        location: "Hokkaido, Japan",
+        latitude: 42.557,
+        longitude: 140.817
+    },
+    {
+        name: "Lake Akan Campsite",
+        location: "Hokkaido, Japan",
+        latitude: 43.384,
+        longitude: 144.099
+    },
+    {
+        name: "Lake Kussharo Campsite",
+        location: "Hokkaido, Japan",
+        latitude: 43.613,
+        longitude: 144.443
+    },
+    {
+        name: "Shikotsu-Toya National Park Campground",
+        location: "Hokkaido, Japan",
+        latitude: 42.688,
+        longitude: 141.346
+    },
+    {
+        name: "Lake Shikaribetsu Kotan Campsite",
+        location: "Hokkaido, Japan",
+        latitude: 43.280,
+        longitude: 143.170
+    },
+    {
+        name: "Furano Campground",
+        location: "Hokkaido, Japan",
+        latitude: 43.348,
+        longitude: 142.383
+    },
+
+    {
+        name: "Noboribetsu Karurusu Campsite",
+        location: "Hokkaido, Japan",
+        latitude: 42.942,
+        longitude: 141.142
+    },
+    {
+        name: "Nasu Kogen Campground",
+        location: "Tochigi, Japan",
+        latitude: 37.098,
+        longitude: 139.999
+    },
+    {
+        name: "Kamikochi Konashidaira Campground",
+        location: "Nagano, Japan",
+        latitude: 36.246,
+        longitude: 137.657
+    },
+    {
+        name: "Norikura Kogen Campsite",
+        location: "Nagano, Japan",
+        latitude: 36.106,
+        longitude: 137.550
+    },
+    {
+        name: "Shiga Kogen Campsite",
+        location: "Nagano, Japan",
+        latitude: 36.740,
+        longitude: 138.500
+    },
+    {
+        name: "Kiso Valley Campground",
+        location: "Nagano, Japan",
+        latitude: 35.842,
+        longitude: 137.690
+    },
+    {
+        name: "Lake Suwa Campsite",
+        location: "Nagano, Japan",
+        latitude: 36.046,
+        longitude: 138.114
+    },
+    {
+        name: "Hakuba Iwatake Campground",
+        location: "Nagano, Japan",
+        latitude: 36.698,
+        longitude: 137.863
+    },
+    {
+        name: "Minakami Outdoor Center Campground",
+        location: "Gunma, Japan",
+        latitude: 36.774,
+        longitude: 138.961
+    },
+    {
+        name: "Oze National Park Campsite",
+        location: "Gunma/Fukushima, Japan",
+        latitude: 36.952,
+        longitude: 139.252
+    },
+
+    {
+        name: "Yamanakako Hananomiyako Campsite",
+        location: "Yamanashi, Japan",
+        latitude: 35.425,
+        longitude: 138.863
+    },
+    {
+        name: "Lake Kawaguchi Campground",
+        location: "Yamanashi, Japan",
+        latitude: 35.517,
+        longitude: 138.751
+    },
+    {
+        name: "Nishizawa Valley Campsite",
+        location: "Yamanashi, Japan",
+        latitude: 35.864,
+        longitude: 138.696
+    },
+    {
+        name: "Aokigahara Forest Campsite",
+        location: "Yamanashi, Japan",
+        latitude: 35.497,
+        longitude: 138.688
+    },
+    {
+        name: "Okutama Lake Campsite",
+        location: "Tokyo, Japan",
+        latitude: 35.809,
+        longitude: 139.096
+    },
+    {
+        name: "Akigawa Valley Campground",
+        location: "Tokyo, Japan",
+        latitude: 35.713,
+        longitude: 139.229
+    },
+    {
+        name: "Tama River Campground",
+        location: "Tokyo, Japan",
+        latitude: 35.706,
+        longitude: 139.367
+    },
+    {
+        name: "Naguri River Campground",
+        location: "Saitama, Japan",
+        latitude: 35.865,
+        longitude: 139.180
+    },
+    {
+        name: "Chichibu Arakawa Camp Village",
+        location: "Saitama, Japan",
+        latitude: 35.990,
+        longitude: 139.085
+    },
+    {
+        name: "Onjuku Beach Campsite",
+        location: "Chiba, Japan",
+        latitude: 35.187,
+        longitude: 140.386
+    },
+
+    {
+        name: "Futtsu Ocean Campground",
+        location: "Chiba, Japan",
+        latitude: 35.305,
+        longitude: 139.806
+    },
+    {
+        name: "Katsuura Beach Campground",
+        location: "Chiba, Japan",
+        latitude: 35.147,
+        longitude: 140.314
+    },
+    {
+        name: "Sawara River Campsite",
+        location: "Chiba, Japan",
+        latitude: 35.893,
+        longitude: 140.498
+    },
+    {
+        name: "Hakone Ashinoko Campsite",
+        location: "Kanagawa, Japan",
+        latitude: 35.212,
+        longitude: 139.025
+    },
+    {
+        name: "Miura Peninsula Campground",
+        location: "Kanagawa, Japan",
+        latitude: 35.142,
+        longitude: 139.618
+    },
+    {
+        name: "Izu Kogen Campground",
+        location: "Shizuoka, Japan",
+        latitude: 34.902,
+        longitude: 139.116
+    },
+    {
+        name: "Shimoda White Beach Campsite",
+        location: "Shizuoka, Japan",
+        latitude: 34.678,
+        longitude: 138.944
+    },
+    {
+        name: "Lake Hamana Campground",
+        location: "Shizuoka, Japan",
+        latitude: 34.740,
+        longitude: 137.600
+    },
+    {
+        name: "Echizen Coast Campground",
+        location: "Fukui, Japan",
+        latitude: 35.999,
+        longitude: 136.073
+    },
+    {
+        name: "Kurobe Gorge Campground",
+        location: "Toyama, Japan",
+        latitude: 36.808,
+        longitude: 137.705
+    },
+
+    {
+        name: "Tateyama Alpine Campsite",
+        location: "Toyama, Japan",
+        latitude: 36.582,
+        longitude: 137.600
+    },
+    {
+        name: "Awaji Island Campground",
+        location: "Hyogo, Japan",
+        latitude: 34.427,
+        longitude: 134.914
+    },
+    {
+        name: "Rokko Mountain Campground",
+        location: "Hyogo, Japan",
+        latitude: 34.777,
+        longitude: 135.243
+    },
+    {
+        name: "Minoh Camp Field",
+        location: "Osaka, Japan",
+        latitude: 34.866,
+        longitude: 135.470
+    },
+    {
+        name: "Setouchi Inland Sea Campsite",
+        location: "Okayama, Japan",
+        latitude: 34.462,
+        longitude: 133.948
+    },
+    {
+        name: "Mount Daisen Campsite",
+        location: "Tottori, Japan",
+        latitude: 35.371,
+        longitude: 133.539
+    },
+    {
+        name: "Izumo Taisha Campground",
+        location: "Shimane, Japan",
+        latitude: 35.401,
+        longitude: 132.685
+    },
+    {
+        name: "Miyajima Campsite",
+        location: "Hiroshima, Japan",
+        latitude: 34.296,
+        longitude: 132.319
+    },
+    {
+        name: "Aso Campground",
+        location: "Kumamoto, Japan",
+        latitude: 32.884,
+        longitude: 131.104
+    },
+    {
+        name: "Kirishima Kogen Camp Village",
+        location: "Kagoshima, Japan",
+        latitude: 31.913,
+        longitude: 130.862
+    }
+];
