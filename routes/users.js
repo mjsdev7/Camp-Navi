f7476bb5eb43506cf93891be378d1d3cae8053d5
mjsdev7 <m.j.swales@hotmail.com>
2026-07-06 17:26:40 +0900

    Add login route with Passport authentication

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 
@@ -20,4 +21,21 @@ router.post('/register', catchAsync(async (req, res) => {
     }
 
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login'
+    }),
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        res.redirect('/campgrounds');
+    }
+);
+
 module.exports = router;
