9faedee904b83f5d99c3921ffcbdfe28adbda0e8
mjsdev7 <m.j.swales@hotmail.com>
2026-07-07 18:38:33 +0900

    Fix returnTo redirect behavior with updated Passport session handling

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -31,13 +32,15 @@ router.get('/login', (req, res) => {
 
 router.post(
     '/login',
+    storeReturnTo,
     passport.authenticate('local', {
         failureFlash: true,
         failureRedirect: '/login'
     }),
     (req, res) => {
         req.flash('success', 'Welcome back!');
-        res.redirect('/campgrounds');
+        const redirectUrl = res.locals.returnTo || '/campgrounds';
+        res.redirect(redirectUrl);
     }
 );
 
