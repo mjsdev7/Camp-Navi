a12e176752c97a1286c2e01ccf6746a755848cf5
mjsdev7 <m.j.swales@hotmail.com>
2026-07-07 12:51:11 +0900

    feat: add isLoggedIn middleware for route protection

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utilities/catchAsync');
 const { campgroundSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 
 const ExpressError = require('../utilities/ExpressErrors');
 const Campground = require('../models/campground');
@@ -31,7 +32,7 @@ router.get('/', catchAsync(async (req, res) => {
 
 
 // NEW FORM
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 });
 
