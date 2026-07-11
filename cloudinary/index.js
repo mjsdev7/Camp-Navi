604a77c5b456395f25f54bc7c23187e16adf79a8
mjsdev7 <m.j.swales@hotmail.com>
2026-07-11 17:49:56 +0900

    Fix Cloudinary storage configuration

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -3,8 +3,8 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_CLOUD_NAME,
-    api_secret: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
 });
 
 const storage = new CloudinaryStorage({
@@ -15,5 +15,5 @@ const storage = new CloudinaryStorage({
 
 module.exports = {
     cloudinary,
-    folder
+    storage
 }
