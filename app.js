2c1bcb85627a9a2e2c6659eee800b2f6c95a3778
mjsdev7 <m.j.swales@hotmail.com>
2026-07-18 18:18:08 +0900

    Add MongoDB injection protection with express-mongo-sanitize"

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const User = require('./models/user');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
+const mongoSanitize = require('express-mongo-sanitize');
 
 
 // DB
@@ -35,7 +36,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(mongoSanitize());
 
 // session config
 const sessionConfig = {
