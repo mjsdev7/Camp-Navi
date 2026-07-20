d0c7b669e5d8bd05c60951cc0d57710780fae6eb
mjsdev7 <m.j.swales@hotmail.com>
2026-07-20 18:16:51 +0900

    Configure Express for Vercel

--- app.js
+++ app.js
@@ -166,7 +166,4 @@ app.use((err, req, res, next) => {
 });
 
 // server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Serving on port ${PORT}`);
-});
+module.exports = app;
