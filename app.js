54160982f20022910c470f39a79807288ab25eda
mjsdev7 <m.j.swales@hotmail.com>
2026-07-23 16:56:26 +0900

    Add Passport session configuration

--- app.js
+++ app.js
@@ -145,6 +145,9 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user || null;
